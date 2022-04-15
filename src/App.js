03596efbf8ca734394c771022b3245198c34b025
lionel <lionellangueh@gmail.com>
2022-04-15 16:54:00 +0200

    not the end ...

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   const [location, setLocation] = useState("");
-  // const [photo, setPhoto] = useState({});
 
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=878c20b66d9ca5286439af1bea6ec548&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=878c20b66d9ca5286439af1bea6ec548&cnt=5&units=metric`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
@@ -18,6 +17,8 @@ function App() {
       setLocation("");
     }
   };
+
+  // Object.entries(data).map((x) => console.log(x));
 
   return (
     <div className="app">
@@ -31,17 +32,25 @@ function App() {
         />
       </div>
 
+      {/* {data.map((x, i=0) => (
+        <div>
+          <h1>{x.main.temp}</h1>
+          <h2>{x.weather[i].main}</h2>
+        </div>
+         return i + 8
+      ))} */}
+
       <div className="container">
         <div className="top">
           <div className="location">
             {data.city ? <p>{data.city.name}</p> : null}
           </div>
           <div className="temp">
-            {data.list ? <h1>{data.list[3].main.temp.toFixed()}°C</h1> : null}
+            {data.list ? <h1>{data.list[0].main.temp.toFixed()}°C</h1> : null}
           </div>
 
           <div className="description">
-            {data.list ? <p>{data.list[3].weather[0].main}</p> : null}
+            {data.list ? <p>{data.list[0].weather[0].main}</p> : null}
           </div>
         </div>
 
@@ -51,29 +60,29 @@ function App() {
               <p>Feels Like : </p>
               {data.list ? (
                 <p className="bold">
-                  {data.list[3].main.feels_like.toFixed()}°C
+                  {data.list[0].main.feels_like.toFixed()}°C
                 </p>
               ) : null}
             </div>
             <div className="humidity">
               <p>Humidity :</p>
               {data.list ? (
-                <p className="bold">{data.list[3].main.humidity}%</p>
+                <p className="bold">{data.list[0].main.humidity}%</p>
               ) : null}
             </div>
             <div className="wind">
               <p>Wind Speed : </p>
               {data.list ? (
-                <p className="bold">{data.list[3].wind.speed.toFixed()} MPH</p>
+                <p className="bold">{data.list[0].wind.speed.toFixed()} MPH</p>
               ) : null}
             </div>
           </div>
         )}
       </div>
-      {/* 
-      <ul>
-        {data.map((datas) => (
-          <li>{datas.list[3].wind.speed}</li>
+
+      {/* <ul>
+        {Object.keys(data).map((datas) => (
+          <li key={datas}>{data[datas].name}</li>
         ))}
       </ul> */}
     </div>
